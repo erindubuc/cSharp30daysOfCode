ff03d8bea3152ee8a0705660c73747fee9a75714
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-01 18:28:05 -0500

    Completed challenge #1: Data Types

--- HackerRank1/Program.cs
+++ HackerRank1/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace HackerRank1
 {
-    class Program
+  
+    class Solution
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+
+            // Declare second integer, double, and String variables.
+            int j;
+            double inputDouble;
+            string inputString;
+
+            // Read and save an integer, double, and String to your variables.
+            j = Convert.ToInt32(Console.ReadLine());
+            inputDouble = Convert.ToDouble(Console.ReadLine());
+            inputString = Console.ReadLine();
+
+            // Print the sum of both integer variables on a new line.
+            int k = i + j;
+            Console.WriteLine(k);
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine("{0:.0}", d + inputDouble);
+
+
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+            Console.WriteLine(s + inputString);
+
+            Console.ReadLine();
 
         }
     }
