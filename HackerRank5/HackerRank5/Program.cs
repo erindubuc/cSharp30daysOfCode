708215e9156f17d7760d9f94cdf01ea7d1438f6f
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-06 15:27:32 -0500

    Created program to print a number, n, times its multiples 1 - 10

---
+++ HackerRank5/HackerRank5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    // Given an integer, n, print its first  multiples. Each multiple should be printed on a new line in the 
+    // form: n x i = result.
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < 11; i++)
+        {
+            int result = n * i;
+            Console.WriteLine("{0} x {1} = {2}", n, i, result);
+        }
+        Console.ReadLine();
+    }
+}
+
