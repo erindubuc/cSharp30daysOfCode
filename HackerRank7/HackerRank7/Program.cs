f3a83bf872f6d46b263214e6110ecd69c6e0c8df
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-12 14:32:39 -0500

    Created program to print an array's elements in reverse order as a single, space-separated line

---
+++ HackerRank7/HackerRank7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    /*
+     * Given an array of n integers, print the array's elements in reverse order as a single line of space-separated numbers.
+    */
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        Array.Reverse(arr);
+        foreach (int num in arr)
+            Console.Write("{0} ", num);
+    }
+}
