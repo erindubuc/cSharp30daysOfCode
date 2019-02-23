0ab9488bd317ebfbdef7acf9396284d444a823a6
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-22 19:26:22 -0500

    Created a constructor and method to find the largest absolute difference in the ints of an array

---
+++ Scope/Scope/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+class Difference
+{
+    private int[] elements;
+    public int maximumDifference;
+
+    //class constructor that takes an array of integers as a param 
+    // and saves it to the  instance variable.
+    public Difference(int[] a)
+    {
+        this.elements = a;
+    }
+    // computeDifference method that finds the maximum absolute difference 
+    // between any 2 numbers in N and stores it in the maxDiff instance variable.
+    public int computeDifference()
+    {
+        // elements in array - sort to find smallest and largest nums
+        int min = elements.Min();
+        int max = elements.Max();
+        maximumDifference = max - min;
+        // subtract their difference to get maxdiff
+        return maximumDifference;
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+        Difference d = new Difference(a);
+
+        d.computeDifference();
+
+        Console.Write(d.maximumDifference);
+    }
+}
