6107913c772351fe0f9d710f0be59e4fb6bc7732
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-25 12:25:04 -0500

    Created insert func to return a ref to the head node of linked list

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+class Node
+{
+    public int data;
+    public Node next;
+    public Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+
+}
+class Solution
+{
+
+    public static Node insert(Node head, int data)
+    {
+        // create a new node
+        Node newNode = new Node(data);
+        // place head into a 'current' node 
+        Node current = head;
+
+        if (head == null)
+        {
+            // get first node
+            return newNode;
+        }
+        else
+        {
+            while (current.next != null)// head/current != null
+            {
+                current = current.next; // current now points to next current 
+            }
+
+        }
+        // pointing to newNode of data
+        current.next = newNode;
+        return head;
+    }
+
+    public static void display(Node head)
+    {
+        Node start = head;
+        while (start != null)
+        {
+            Console.Write(start.data + " ");
+            start = start.next;
+        }
+    }
+    static void Main(String[] args)
+    {
+
+        Node head = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head, data);
+        }
+        display(head);
+        Console.ReadLine();
+    }
+}
