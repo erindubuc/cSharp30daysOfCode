22b5a0697b7865cf2c2dd3ffa11b0f8280917c4d
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-02-26 15:58:54 -0500

    Print int value of string, or Bad String if exception

---
+++ ExceptionsStringToInt/ExceptionsStringToInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string S = Console.ReadLine();
+
+        try
+        {
+            Console.WriteLine(Int32.Parse(S));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bad String");
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
+
